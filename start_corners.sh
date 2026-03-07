ff491525b44abbd76eb67928231999ff55815fe2
DanDan <dandan7932@hotmail.com>
2026-03-07 17:58:07 +0000

    Update start_corners.sh

--- start_corners.sh
+++ start_corners.sh
@@ -3,5 +3,5 @@ if [ ! -d "venv" ]; then
     python3 -m venv corner-venv
 fi
 source corner-venv/bin/activate
-pip install pillow screeninfo
+pip install pillow screeninfo tkinter
 python3 rounded_corners.py "$@"
